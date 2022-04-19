c6436125c75c91b438d21e732540677b476634bc
Christian Alcalde <cc.alcalde@yahoo.com>
2022-04-19 16:55:51 -0700

    added schema for our post route

--- routes/shipments.js
+++ routes/shipments.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const router = new express.Router();
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schema/orderSchema.json");
 
 const { shipProduct } = require("../shipItApi");
 
@@ -14,10 +16,15 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  const { productId, name, addr, zip } = req.body;
-  const shipId = await shipProduct({ productId, name, addr, zip });
+  console.log(req.body);
+  const result = jsonschema.validate(req.body, orderSchema);
+  if (!result.valid) {
+    let errors = result.errors.map((err) => err.stack);
+    throw new BadRequestError(errors);
+  }
+
+  const shipId = await shipProduct(req.body);
   return res.json({ shipped: shipId });
 });
-
 
 module.exports = router;
