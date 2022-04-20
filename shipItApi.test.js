db4986eef8cdee505e137eaa3372d3c7b92b5769
Dong Hyun Choi <dongandrewchoi@gmail.com>
2022-04-19 17:50:04 -0700

    THE MOCK TEST

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,17 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
 
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
 
 test("shipProduct", async function () {
+  axiosMock.onPost(`${SHIPIT_SHIP_URL}`).reply(200, {
+    receipt: {
+      shipId: 6172,
+    },
+  });
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +19,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(6172);
 });
